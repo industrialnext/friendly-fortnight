0553500a5f1b3bbb24cf91c2c2945a87cbb77932
shoelick <shoelick@users.noreply.github.com>
2024-02-02 13:09:32 -0800

    Update install_industrialnext_deps.sh

--- install_industrialnext_deps.sh
+++ install_industrialnext_deps.sh
@@ -6,6 +6,6 @@ ENV_NAME=${ENV_NAME:py310}
 PYTHON_BIN=${PYTHON_BIN:=$HOME/.pyenv/versions/$ENV_NAME/bin/python}
 
 # Ensure we have the credentials available
-[ -n "$INDUSTRIALNEXT_PY_INDEX" ] || { echo "Error: 'INDUSTRIALNEXT_PY_INDEX' is not set. Please see the README.md" >&2; exit 1; }
+[ -n "$INDUSTRIALNEXT_PY_INDEX" ] || { echo "Error: 'INDUSTRIALNEXT_PY_INDEX' is not set. Please see the documentation" >&2; exit 1; }
 
 ${PYTHON_BIN} -m pip install -i ${INDUSTRIALNEXT_PY_INDEX} -r requirements.txt
